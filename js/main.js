bce70454df4ef1475697d7b7c93afb338dd29fe2
Vasya <chatterbox1995@mail.ru>
2019-07-01 22:06:00 +0300

    add some css animation + js modal
    add some css animation + js modal

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@ $(document).ready(function() {
     $("a.scrollto").click(function () {
         var elementClick = '#'+$(this).attr("href").split("#")[1]
         var destination = $(elementClick).offset().top;
-        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1200);
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1900);
         return false;
     });
+});
+
+var hidden = document.querySelector('.hidden');
+var btnCall = document.querySelector('.act');
+btnCall.addEventListener('click', function(){
+hidden.classList.remove('hide');
+hidden.classList.add('active');
+});
+var btnExit = document.querySelector('.exit');
+btnExit.addEventListener('click', function(){
+hidden.classList.add('hide');
+hidden.classList.remove('active');
 });
